2355706bdb3dac3c9f2b0e12ce8360ceffef18ef
Roman <chidori031@mail.ru>
2025-06-13 17:15:39 +0300

    upd structure

--- services/object_detection.py
+++ services/object_detection.py
@@ -1,10 +1,10 @@
-from ultralytics import YOLO
-
-# Load a model
-model = YOLO("C:\Projects\card-style-generator\models\yolo11x.pt")
-
-# Perform object detection on an image
-results = model(r"C:\Users\romac\OneDrive\Рабочий стол\nsfwe\asd.jpg")
-results[0].show()
-
-print(results[0])
+# from ultralytics import YOLO
+#
+# # Load a model
+# model = YOLO("C:\Projects\card-style-generator\models\.pt")
+#
+# # Perform object detection on an image
+# results = model(r"C:\Users\romac\OneDrive\Рабочий стол\nsfwe\asd.jpg")
+# results[0].show()
+#
+# print(results[0])
